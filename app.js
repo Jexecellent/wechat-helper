26b11acd382d9d43f178f6985ee907bdef6c3d46
Bearn <592374752@qq.com>
2017-09-11 17:24:48 +0800

    增加node服务模块

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // import express from 'express';
 // import bodyParser from 'body-parser';
-// import db from './db';
-//const db = require('./db');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -19,26 +18,7 @@ router.use((req, res, next) => {
 router.route('/api')
       .get((req, res) => {
         res.send('hello world!')
-       /* db.Articles.find((err, articles) => {
-          if (err) {
-            res.send(err);
-            return;
-          }
-          // articles.forEach( article => {
-          //   article.date = moment(objectidToTimestamp(article._id)).format('YYYY-MM-DD');
-          // } );
-
-          res.json(articles);
-        });*/
       })
-
-     /* .post((req, res) => {
-        new db.Articles(req.body.postInformation).save((err) => {
-          if (err) res.send(err);
-          res.json({ message: 'new article has been created' });
-        });
-      });*/
-
 
 app.use('/', router);
 
